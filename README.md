b5032fb032a555ae9d0399a4227f5f00946d041a
Benii1 <szlonkaibenedek@gmail.com>
2023-05-23 18:23:21 +0200

    Main page

--- README.md
+++
@@ -1,1 +0,0 @@
-# bbenii1.github.io
--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-    /*function videoPlay() {
-        var video = document.getElementById("video");
-        video.volume = 0.2, 0;
-        video.play()
-    }
-    
-    document.getElementById('video').play();
-    document.getElementById('video').muted=false;
-    document.getElementById('video').volume=0.1;
-    
-    function toggleMute() {
-    var myAudio = document.getElementById('video');
-    myAudio.muted = !myAudio.muted;
-    }
+$('.txt').html(function(i, html) {
+  var chars = $.trim(html).split("");
 
-    var unmute = document.getElementById('unmute');
-    console.log(unmute);
-    if(unmute){
-        unmute.addEventListener('click', function(unmute){
-            unmute.target.classList.toggle('mute');
-    }) 
-    }*/
+  return '<span>' + chars.join('</span><span>') + '</span>';
+});
